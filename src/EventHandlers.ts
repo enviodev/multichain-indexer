22b1c724e4bf2fec32e4c31f8ad3a3c6932ef9f7
nikbhintade <29760727+nikbhintade@users.noreply.github.com>
2025-11-11 12:43:38 +0000

    removed unnecessary handlers & updated config

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -3,38 +3,14 @@
  */
 import {
   UniswapV3Factory,
-  UniswapV3Factory_FeeAmountEnabled,
-  UniswapV3Factory_OwnerChanged,
   UniswapV3Factory_PoolCreated,
 } from "generated";
 
-UniswapV3Factory.FeeAmountEnabled.handler(async ({ event, context }) => {
-  const entity: UniswapV3Factory_FeeAmountEnabled = {
-    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    fee: event.params.fee,
-    tickSpacing: event.params.tickSpacing,
-  };
-
-  context.UniswapV3Factory_FeeAmountEnabled.set(entity);
-});
-
-UniswapV3Factory.OwnerChanged.handler(async ({ event, context }) => {
-  const entity: UniswapV3Factory_OwnerChanged = {
-    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    oldOwner: event.params.oldOwner,
-    newOwner: event.params.newOwner,
-  };
-
-  context.UniswapV3Factory_OwnerChanged.set(entity);
-});
 
 UniswapV3Factory.PoolCreated.handler(async ({ event, context }) => {
   const entity: UniswapV3Factory_PoolCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    token0: event.params.token0,
-    token1: event.params.token1,
-    fee: event.params.fee,
-    tickSpacing: event.params.tickSpacing,
+    chainId: event.chainId,
     pool: event.params.pool,
   };
 
